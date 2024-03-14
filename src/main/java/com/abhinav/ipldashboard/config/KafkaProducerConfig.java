0924e0d86d68c578c924f22ab147dae4452286b2
Abhinav Prabhat <abhinavprabhat02@gmail.com>
2024-03-14 14:38:28 +0530

    pushed to kafka(using MatchModel as value)

--- src/main/java/com/abhinav/ipldashboard/config/KafkaProducerConfig.java
+++ src/main/java/com/abhinav/ipldashboard/config/KafkaProducerConfig.java
@@ -1,30 +1,32 @@
 package com.abhinav.ipldashboard.config;
 
 import com.abhinav.ipldashboard.entity.MatchModel;
-import com.fasterxml.jackson.databind.JsonSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration
 public class KafkaProducerConfig {
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, MatchModel> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
+    public KafkaTemplate<String, MatchModel> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
--- src/main/java/com/abhinav/ipldashboard/service/KafkaProducerService.java
+++ src/main/java/com/abhinav/ipldashboard/service/KafkaProducerService.java
@@ -11,47 +11,17 @@ import java.util.List;
 
 @Service
 public class KafkaProducerService {
-    /*
 
-    private static final String TOPIC_NAME = "ipl";
-    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
-    private KafkaProducer<String, MatchModel> producer;
-
-    public KafkaProducerService() {
-        Properties props = new Properties();
-        props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.JavaSerializer");
-
-        producer = new KafkaProducer<>(props);
-    }
-
-    public void sendToKafka(List<MatchModel> modelList) {
-        try {
-            for (MatchModel model : modelList) {
-                ProducerRecord<String, MatchModel> record = new ProducerRecord<>(TOPIC_NAME, model);
-                producer.send(record);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            producer.close();
-        }
-    }
-
-
-     */
-
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, MatchModel> kafkaTemplate;
 
     @Autowired
-    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducerService(KafkaTemplate<String, MatchModel> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendToKafka(List<MatchModel> matchModels) {
         for (MatchModel matchModel : matchModels) {
-            kafkaTemplate.send("ipl", matchModel.getId(), String.valueOf(matchModel));
+            kafkaTemplate.send("ipl", matchModel.getId(), matchModel);
         }
     }
 }
